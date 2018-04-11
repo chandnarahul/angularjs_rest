a91a89499010e957e56e8357fb34570c5d4ae886
rahul.chandna <chandnarahul@yahoo.com>
2018-04-11 22:59:46 +0200

    added comments

--- controller/postDataController.js
+++ controller/postDataController.js
@@ -1,6 +1,7 @@
 var restApiDemo=angular.module('postData');
 
 restApiDemo.component('postData', {
+    //templateUrl:'templates/post_data.html'
     template:'<table>'+
     '<tr ng-repeat="post in posts">'+
         '<td>{{post.userId}}</td>'+
@@ -10,10 +11,15 @@ restApiDemo.component('postData', {
         '<td><input type="button" value="delete"></td>'+
     '</tr>'+
     '</table>',
-    controller: function postDataController($scope, $http) {
+    controller: 
+    function postDataController($scope, $http) {
+        //this will execute on controller load
         $http.get('https://jsonplaceholder.typicode.com/posts').
             then(function(response) {
                 $scope.posts = response.data;
             });
+        
+        
+        
     }
 });
