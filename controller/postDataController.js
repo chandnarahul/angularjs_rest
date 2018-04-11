137a6e911718a2947eca311b2f0174b2340cc7b1
rahul.chandna <chandnarahul@yahoo.com>
2018-04-11 22:36:57 +0200

    renamed view to templates

--- controller/postDataController.js
+++ controller/postDataController.js
@@ -1,7 +1,15 @@
 var restApiDemo=angular.module('postData');
 
 restApiDemo.component('postData', {
-    template:'/views/post_data.html',
+    template:'<table>'+
+    '<tr ng-repeat="post in posts">'+
+        '<td>{{post.userId}}</td>'+
+        '<td>{{post.id}}</td>'+
+        '<td>{{post.title}}</td>'+
+        '<td>{{post.body}}</td>'+
+        '<td><input type="button" value="delete"></td>'+
+    '</tr>'+
+    '</table>',
     controller: function postDataController($scope, $http) {
         $http.get('https://jsonplaceholder.typicode.com/posts').
             then(function(response) {
