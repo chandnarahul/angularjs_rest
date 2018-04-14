ea20c2728c1ee5d05c2eeee7aef86b1f80328967
rahul.chandna <chandnarahul@yahoo.com>
2018-04-14 16:48:11 +0200

    added data check and moved config inside module

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var restApiDemo = angular.module('restApiDemo', ['ngRoute', 'postData', 'albumData']);
 
-restApiDemo.config(function ($routeProvider) {
+postData.config(function ($routeProvider) {
     $routeProvider
         .when("/home", {
             templateUrl: "templates/blank.html"
-        })
-        .when("/admin/:adminId", {
-            templateUrl: "templates/default_view.html",
-        })
-        .when("/admin", {
-            templateUrl: "templates/default_view.html"
         });
 });
--- component/postDataComponent.js
+++ component/postDataComponent.js
@@ -1,4 +1,4 @@
-var postData = angular.module('postData',['ngRoute']);
+var postData = angular.module('postData', ['ngRoute']);
 
 postData.component('postData', {
     templateUrl: 'templates/post_data.html',
@@ -10,13 +10,34 @@ postData.component('postData', {
                     $scope.post = response.data;
                 });
                 */
-            console.log($routeParams);
-            $scope.post = { "userId": 2, "id": 12, "title": "in quibusdam tempore odit est dolorem", "body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio" };
+
+            if (!$routeParams.adminId) {
+                //$location.path('/somewhereElse');
+                console.log('$routeParams is empty');
+            } else {
+                console.log('$routeParams is ' + $routeParams.adminId);
+            }
+            $scope.post = {
+                "userId": 2,
+                "id": 12,
+                "title": "in quibusdam tempore odit est dolorem",
+                "body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+            };
 
             $scope.updatePost = function (id) {
-                $scope.post = { "userId": 12, "id": 112, "title": "updated", "body": "updated on [" + new Date() + "]" };
+                $scope.post = {"userId": 12, "id": 112, "title": "updated", "body": "updated on [" + new Date() + "]"};
             }
 
         }
 
+});
+
+postData.config(function ($routeProvider) {
+    $routeProvider
+        .when("/admin/:adminId", {
+            templateUrl: "templates/default_view.html",
+        })
+        .when("/admin", {
+            templateUrl: "templates/default_view.html"
+        });
 });
